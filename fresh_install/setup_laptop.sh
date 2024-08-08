efb5a641fe2ceabb72d64e0a9227dc09b7531a06
Ricardo Chaves <ricardochaves@ua.pt>
2024-08-08 09:29:01 +0100

    fresh_install: install tree apt package

--- fresh_install/setup_laptop.sh
+++ fresh_install/setup_laptop.sh
@@ -90,7 +90,7 @@ configure_gnome_general() {
 }
 
 setup_apt() {
-    pkgs="curl vim git terminator meld jq bat lm-sensors htop moreutils cpufrequtils appimagelauncher python3-pip picocom sshpass"
+    pkgs="curl vim git terminator meld jq bat lm-sensors htop moreutils cpufrequtils appimagelauncher python3-pip picocom sshpass tree"
     to_install=""
     for pkg in $pkgs; do
         c_blue "Checking if $pkg is installed"
